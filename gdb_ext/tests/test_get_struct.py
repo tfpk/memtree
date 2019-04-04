43a84ca0cbc29682aaac154e53ccb2b611754acb
Tom Kunc <tomkunc0@gmail.com>
2019-04-04 22:45:27 +1100

    test(get_struct): update struct tests

--- gdb_ext/tests/test_get_struct.py
+++ gdb_ext/tests/test_get_struct.py
@@ -7,17 +7,16 @@ def test_get_struct():
 
     o = get_output(
         """
-        #define NULL 0x0
-        int main(void){
-            struct s {
-                int i;
-                char c;
-                int ints[4];
-            };
-            struct s s = {1, 'c', {5,6,7,8}};
-            struct s sl[2] = {{1, 'c', {5,6,7,8}}, {1, 'd', {9,6,4,2}}};
-            return 0; // break
-        }
+int main(void){
+    struct s {
+        int i;
+        char c;
+        int ints[4];
+    };
+    struct s s = {1, 'c', {5,6,7,8}};
+    struct s sl[2] = {{1, 'c', {5,6,7,8}}, {1, 'd', {9,6,4,2}}};
+    return 0; // break
+}
         """,
         """
 import gdb_interface
